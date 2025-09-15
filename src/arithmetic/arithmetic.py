dab4ba52e6908fffdbe0da7ff6486bceaa593da5
Victor Guo <admin@beulahworks.com>
2025-09-14 22:50:35 -0500

    passed all tests

--- src/arithmetic/arithmetic.py
+++ src/arithmetic/arithmetic.py
@@ -1,6 +1,6 @@
 """arithmetic functions."""
-
 from __future__ import annotations
+import math
 
 
 def add_numbers(a: int, b: int) -> int:
@@ -13,7 +13,7 @@ def add_numbers(a: int, b: int) -> int:
     Returns:
         Sum of a and b.
     """
-    raise NotImplementedError
+    return a + b
 
 
 def factorial(n: int) -> int:
@@ -28,7 +28,9 @@ def factorial(n: int) -> int:
     Raises:
         ValueError: if n is negative
     """
-    raise NotImplementedError
+    if n < 0:
+        raise ValueError()
+    return math.factorial(n)
 
 
 def is_prime(n: int) -> bool:
@@ -43,4 +45,9 @@ def is_prime(n: int) -> bool:
     Returns:
         True if n is prime; otherwise False.
     """
-    raise NotImplementedError
+    if n <= 1:
+        return False
+    for i in range(2, int(math.sqrt(n))+1):
+        if n % i == 0:
+            return False
+    return True
