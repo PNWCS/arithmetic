6a60d5370d9ce08986064f0fada3d3eaa30accd6
Sai Karthik <saikarthik.regilla@gmail.com>
2025-09-15 19:06:08 +0000

    Added all the arithematic functions

--- src/arithmetic/arithmetic.py
+++ src/arithmetic/arithmetic.py
@@ -13,7 +13,8 @@ def add_numbers(a: int, b: int) -> int:
     Returns:
         Sum of a and b.
     """
-    raise NotImplementedError
+    return a + b
+    # raise NotImplementedError
 
 
 def factorial(n: int) -> int:
@@ -28,7 +29,16 @@ def factorial(n: int) -> int:
     Raises:
         ValueError: if n is negative
     """
-    raise NotImplementedError
+    if n < 0:
+        raise ValueError
+    else:
+        if n == 0:
+            return 1
+        else:
+            fact = 1
+            for i in range(1, n + 1):
+                fact = fact * i
+            return fact
 
 
 def is_prime(n: int) -> bool:
@@ -43,4 +53,10 @@ def is_prime(n: int) -> bool:
     Returns:
         True if n is prime; otherwise False.
     """
-    raise NotImplementedError
+    if n <= 1:
+        return False
+    else:
+        for i in range(2, n):
+            if n % i == 0:
+                return False
+        return True
