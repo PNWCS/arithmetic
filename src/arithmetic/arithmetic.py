c6110c5152f9528355e7dfa6ad238fa880404978
Victor Guo <admin@beulahworks.com>
2025-09-14 23:10:11 -0500

    formatted file

--- src/arithmetic/arithmetic.py
+++ src/arithmetic/arithmetic.py
@@ -1,5 +1,7 @@
 """arithmetic functions."""
+
 from __future__ import annotations
+
 import math
 
 
@@ -47,7 +49,7 @@ def is_prime(n: int) -> bool:
     """
     if n <= 1:
         return False
-    for i in range(2, int(math.sqrt(n))+1):
+    for i in range(2, int(math.sqrt(n)) + 1):
         if n % i == 0:
             return False
     return True
